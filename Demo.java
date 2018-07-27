fed983c650d65bf2b6efcca823cdea9ce861e8ec
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-07-27 11:34:25 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -5,10 +5,14 @@ public static void main(String args[])
 char ch;
 System.out.println("enter the character:");
 Scanner sc=new Scanner(System.in);
-ch=sc.nextChar();
+ch=sc.next().charAt(0);
 if((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
 {
 System.out.println("given character is vowel");
 }
+  else
+  {
+    System.out.println("given character is consonent");
+  }
 }
 }
