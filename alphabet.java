528bef291b607535e7bfb23bdcb0f20e9b199743
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-07-27 11:54:19 +0530

    Update alphabet.java

--- alphabet.java
+++ alphabet.java
@@ -5,11 +5,11 @@ public static void main(String args[])
 char c='a';
 if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
 {
-System.out.println(c+ "is an alphabet");
+System.out.println("Alphabet");
 }
 else
 {
-System.out.println(c+"is not an alphabet");
+System.out.println("No");
 }
 }
 }
