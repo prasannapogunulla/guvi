3810b86f2f2c6aaa6af9c155f7b3cc43b86ab4c5
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-07-27 15:08:29 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Demo
+{
+public static void main(String args[])
+{
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+String line=br.readLine();
+int N=Integer.parseInt(line);
+for(int i=0;i<N;i++)
+{
+System.out.println("Hello");
+}
+}
