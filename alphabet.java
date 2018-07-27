45f1d2bd1b67614ce06d4d5dc65563af0f2fded7
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-07-27 11:52:03 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,15 @@
+public class Demo
+{
+public static void main(String args[])
+{
+char c='a';
+if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
+{
+System.out.println(c+ "is an alphabet");
+}
+else
+{
+System.out.println(c+"is not an alphabet");
+}
+}
+}
