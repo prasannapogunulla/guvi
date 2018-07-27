bd5f343e6be1236cbfccee6beab4b07b3fa92f59
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-07-27 13:06:13 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,35 @@
+public class Demo
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int year=sc.nextInt();
+boolen flag=false;
+if(year%400==0)
+{
+flag=true;
+}
+else if(year%100==0)
+{
+flag=flase;
+}
+else if(year%4==0)
+{
+flag=true
+}
+else
+{
+flag=false;
+}
+if(flag)
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
+
+
