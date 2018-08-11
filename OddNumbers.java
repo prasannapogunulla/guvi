3d76319773c76ab11fd0b49bc3fc063cf37ff6ce
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-11 10:52:26 +0530

    Update OddNumbers.java

--- OddNumbers.java
+++ OddNumbers.java
@@ -3,8 +3,8 @@ class OddNumbers
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-  int a=nextInt();
-  int b=nextInt();
+  int a=sc.nextInt();
+  int b=sc.nextInt();
   for(int i=a+1;i<b;i++)
   {
     if(i%2!=0)
