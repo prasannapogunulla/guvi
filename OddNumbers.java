fe7c2439e6da53cbb48128eb40a342a0fc3d1a02
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-11 10:32:38 +0530

    Create OddNumbers.java

---
+++ OddNumbers.java
@@ -0,0 +1,23 @@
+class OddNumbers
+{
+public static void main(String args[])
+{
+int num1=5,num2=20,count;
+System.out.println("odd numbers between(10-20):");
+for(count=10;count<=num2;count++)
+{
+if(count%2==1)
+{
+System.out.println(count);
+}
+}
+System.out.println("odd numbers between(1-5):");
+for(count=1;count<=num1;count++)
+{
+if(count%2==1)
+{
+System.out.println(count);
+}
+}
+}
+}
