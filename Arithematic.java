66cb40edd1a2b8363948b22f365648ca02f1eee4
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-13 10:03:40 +0530

    Create Arithematic.java

---
+++ Arithematic.java
@@ -0,0 +1,11 @@
+class Arithematic
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a=sc.nextInt();
+int d=sc.nextInt();
+System.out.println((n*((2*a)+(n-1)*d))/2);
+}
+}
