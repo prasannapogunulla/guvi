95a2a47407fcb7f5448c211c230c36cbe922bbdf
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-13 09:46:13 +0530

    Update Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -2,10 +2,11 @@ class Multiplication
 {
 public static void main(String args[])
 {
-int n=1,c;
-for(c=1;c<=5;c++)
+Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+for(int c=1;c<=5;c++)
 {
-System.out.println(n+"*"+c+"="+(n*c));
+System.out.println((n*c)+" ");
 }
 }
 }
