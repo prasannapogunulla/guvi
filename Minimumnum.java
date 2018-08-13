72556799529dab3ad90094aec96d4e63eda1301e
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-13 12:59:06 +0530

    Create Minimumnum.java

---
+++ Minimumnum.java
@@ -0,0 +1,23 @@
+class Minimum
+{
+public static void main(String args[])
+{
+int n,min;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+min=a[0];
+for(int i=0;i<n;i++)
+{
+if(min>a[i])
+{
+min=a[i];
+}
+}
+System.out.println(min);
+}
+}
