507dcfea1042c1e4c846ed7f7a9d416246e025d9
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-13 21:06:25 +0530

    Update Ascending.java

--- Ascending.java
+++ Ascending.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class Ascending
 {
-public class static void main(String args[])
+public static void main(String args[])
 {
 int n,temp;
 Scanner sc=new Scanner(System.in);
