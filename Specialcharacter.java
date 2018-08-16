8c99edb6038211bd92eef330d7295831978c490a
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-16 11:52:09 +0530

    Create Specialcharacter.java

---
+++ Specialcharacter.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+class Specialcharacter
+{
+public static String password="";
+public static final String FINAL_CHAR_REGEX="[!@#$%^&*()[\\\\]|;',./{}\\\\\\\\:\\\"<>?]";
+public static void main(String args[])throws IOException
+{
+BufferedReader dataIn=new BufferedReader(new InputStreamReader(System.in));
+password=dataIn.readLine();
+for(int i=0;i<password.length();i++)
+{
+}
+int specialCharCount=password.split(FINAL_CHAR_REGEX,-1).length-1;
+System.out.println(specialCharCount);
+}
+}
