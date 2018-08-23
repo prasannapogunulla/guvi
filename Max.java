f6304a458bdc6c000e484d325630d9c1e1ccf056
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-23 14:54:53 +0530

    Create Max.java

---
+++ Max.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Max
+{
+public static void main(String args[])
+{
+int n,max;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+int a[]=new int[10];
+for(int i=0;i<10;i++)
+{
+a[i]=s.nextInt();
+}
+max=a[0];
+for(int i=0;i<10;i++)
+{
+if(max<a[i])
+{
+max=a[i];
+}
+}
+System.out.println(max);
+}
+}
