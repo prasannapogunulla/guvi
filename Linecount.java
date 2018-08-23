755b0e10c4ca58520fb60b78b3d5acbc74ae5da4
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-23 15:26:21 +0530

    Create Linecount.java

---
+++ Linecount.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Linecount
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+int n=sc.nextInt();
+for(int i=0;i<=n;i++)
+{
+System.out.println(s);
+}
+}
+}
