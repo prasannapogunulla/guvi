5fe6e005899d7693a94cee38c59fbac67ee9b5cc
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-23 14:33:21 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Fibonacci
+{
+public static void main(String args[])
+{
+int t1=1,t2=1;
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=1;i<=n;i++)
+{
+System.out.print(t1+" ");
+int sum=t1+t2;
+t1=t2;
+t2=sum;
+}
+}
+}
