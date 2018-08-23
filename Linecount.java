8d305bfaeef076ebdf4231d765bfffd61cd07ccd
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-23 15:31:56 +0530

    Update Linecount.java

--- Linecount.java
+++ Linecount.java
@@ -6,7 +6,7 @@ public static void main(String args[])
 Scanner sc=new Scanner(System.in);
 String s=sc.next();
 int n=sc.nextInt();
-for(int i=0;i<=n;i++)
+for(int i=1;i<=n;i++)
 {
 System.out.println(s);
 }
