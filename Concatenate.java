7573db834598b5f3868db268bac55d38f0fcc487
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-24 09:48:09 +0530

    Create Concatenate.java

---
+++ Concatenate.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Concatenate
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String firststr=sc.next();
+String secondstr=sc.next();
+System.out.println(firststr+ secondstr);
+}
+}
