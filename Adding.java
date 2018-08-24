1137486454add23036b8b91156ebc625e8635b65
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-24 10:20:10 +0530

    Create Adding.java

---
+++ Adding.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Adding
+{
+public static void main(String args[])
+{
+int m,p,i;
+Scanner sc=new Scanner(System.in);
+m=sc.nextInt();
+p=sc.nextInt();
+i=m+p;
+System.out.println(i);
+}
+}
