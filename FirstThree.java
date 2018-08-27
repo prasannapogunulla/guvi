2315c46e67c43729c96501af77c24fa88f857b1e
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-27 16:13:15 +0530

    Create FirstThree.java

---
+++ FirstThree.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class FirstThree
+{
+public static void main(String args[])
+{
+String str;
+Scanner sc=new Scanner(System.in);
+str=sc.nextLine();
+System.out.println(str.substring(0,3));
+}
+}
