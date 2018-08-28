5511a22ab7db8dfd4644ff72e6e878d55e91ddf9
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-28 09:54:14 +0530

    Create AllDigitsInSameOrder.java

---
+++ AllDigitsInSameOrder.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class  AllDigitsInSameOrder
+{
+	public static void main(String[] args) 
+	{
+    Scanner in = new Scanner(System.in);
+    int Number = in.nextInt(); 
+    int tall = 1;
+    for(int t = 0; tall <=3; tall++)
+    {
+            System.out.print(" " + tall);
+    }   
+}
+}
