f83a6cad494a7de80534ed4d5eea05c247022530
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-28 15:52:39 +0530

    Create GreaterString.java

---
+++ GreaterString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner
+class GreaterString
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s1=sc.next();
+String s2=sc.next();
+if(s1.compareTo(s2)>0)
+System.out.println(s1);
+else if(s1.compareTo(s2)<0)
+System.out.println(s2);
+else
+System.out.println(s1);
+}
+}
