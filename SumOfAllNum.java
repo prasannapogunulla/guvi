364d3a90260b7640b2bcf83ce068c3ae0411d0f6
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-29 15:01:59 +0530

    Create SumOfAllNum.java

---
+++ SumOfAllNum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ class SumOfAllNum
+{
+	public static void main(String arg[])	
+	{
+                  int n,sum=0;
+                  
+                  Scanner sc=new Scanner(System.in);
+ 
+	    
+                   n=sc.nextInt();
+                   int a[]=new int[n]; 
+	    
+                   for(int i=0;i<n;i++)
+                   {      
+                           a[i]=sc.nextInt();
+                   }
+                   for(int i=0;i<n;i++)
+                   {
+                           sum+=a[i];
+                   }  	    
+                   System.out.println(sum);                  
+              	}
+} 
