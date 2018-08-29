102ea8cee0edcb84d0948d2a1e7948bf69c3b38a
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-29 10:56:04 +0530

    Create DifferenceEvenOrOdd.java

---
+++ DifferenceEvenOrOdd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class DifferenceEvenOrOdd
+{
+public static void main(String args[])
+{
+int num1,num2,sum;
+Scanner scan=new Scanner(System.in);
+num1=scan.nextInt();
+num2=scan.nextInt();
+sum=num1-num2;
+if(sum%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
