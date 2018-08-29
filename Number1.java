ca8ddbfac044312aacb537366663dbc8a96379bf
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-29 22:51:31 +0530

    Create Number1.java

---
+++ Number1.java
@@ -0,0 +1,37 @@
+ import java.util.Scanner;
+ DigitsOpr
+{
+private int num;
+public void getNum(int x)
+{
+num=x;
+}
+
+public int productDigits()
+{
+int n,pro;
+n=num;//keep value of num safe
+pro=1;
+while(n>0)
+{
+pro *=(n%10); //find digit using n=n%10 
+n/=10;
+}
+return pro;
+}
+}
+
+ class number1
+{
+public static void main(String []s)
+{
+DigitsOpr dig=new DigitsOpr();
+int n;
+Scanner sc=new Scanner(System.in);
+
+n=sc.nextInt();
+dig.getNum(n);
+
+System.out.println(dig.productDigits());
+}
+}
