0c87fc51be1d73ee11c5dae9f40f016146cfb4e7
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-08-30 15:22:18 +0530

    Create ThreeNumbers.java

---
+++ ThreeNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class ThreeNumbers.java
+{
+public static void main(String args[])
+{
+int a,b,c,res;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+c=sc.nextInt();
+res=(a*b)/c;
+System.out.println(res);
+}
+}
