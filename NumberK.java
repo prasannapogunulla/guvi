18e5e4bd69c467db1bb2bb5618354acf758c5ffa
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-01 09:52:10 +0530

    Create NumberK.java

---
+++ NumberK.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class NumberK
+{
+public static void main(String args[])
+{
+int n,k;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+k=sc.nextInt();
+System.out.println(k);
+}
+}
