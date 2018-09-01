1c9d8a7368c4613843f11d9529732dfdf832ed4d
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-01 15:34:29 +0530

    Update FirstThree.java

--- FirstThree.java
+++ FirstThree.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+import java.io.*;
 class FirstThree
 {
 public static void main(String args[])
 {
 String str;
 Scanner sc=new Scanner(System.in);
-str=sc.nextLine();
-System.out.println(str.substring(0,3));
+str=sc.next();
+  int n=sc.nextInt();
+System.out.println(str.substring(0,n));
 }
 }
