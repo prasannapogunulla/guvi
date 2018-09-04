55596a1197d03e89027610264d03c795be1fef8d
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-04 10:15:37 +0530

    Create Multiples.java

---
+++ Multiples.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Multiple
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int j=sc.nextInt();
+if(j%13==0)
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
