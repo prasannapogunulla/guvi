2730efb2f9d33ee730f5f567e4ce22c98b91d53d
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-04 16:28:20 +0530

    Create Kabali.java

---
+++ Kabali.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Subtraction
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c;
+c=b-a;
+System.out.println(c);
+}
+}
