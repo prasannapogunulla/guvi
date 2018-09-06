54c635d62621398b8c4c7eef8f598cafe5f49c60
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-06 09:45:39 +0530

    Create StringIsNumberOrNot.java

---
+++ StringIsNumberOrNot.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class StringIsNumberOrNot
+{
+    public static void main(String[] args) 
+    {
+
+        String string;
+        Scanner sc=new Scanner(System.in);
+        string=sc.nextLine();
+        boolean numeric = true;
+
+        try {
+            Double num = Double.parseDouble(string);
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+
+        if(numeric)
+            System.out.println("yes");
+        else
+            System.out.println("no");
+    }
+}
