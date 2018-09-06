50bb51e74f587cd49812cfbd38c19531116e31b5
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-06 16:14:55 +0530

    Create DivisionSpace.java

---
+++ DivisionSpace.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class DivisionSpace
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+long a=sc.nextLong();
+long b=sc.nextLong();
+long c;
+c=a/b;
+System.out.println(c);
+}
+}
