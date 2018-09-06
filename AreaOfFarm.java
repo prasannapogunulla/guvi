9a69e163228f30978cee23f2448562ee35f3afc5
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-06 16:10:59 +0530

    Update AreaOfFarm.java

--- AreaOfFarm.java
+++ AreaOfFarm.java
@@ -6,8 +6,8 @@ class AreaOfFarm
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-double a=sc.nextDouble;
-double b=sc.nextDouble;
+double a=sc.nextDouble();
+double b=sc.nextDouble();
 double c;
 c=a*b;
 System.out.format("%.5f",c);
