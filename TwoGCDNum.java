a5449bd118b30f3ef3f39fddea108ccf7d12f145
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-06 16:28:44 +0530

    Create TwoGCDNum.java

---
+++ TwoGCDNum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class TwoGCDNum
+{
+public static void main(String args[])
+{
+int n1,n2,gcd=1;
+Scanner sc=new Scanner(System.in);
+n1=sc.nextInt();
+n2=sc.nextInt();
+for(int i=1;i<=n1 && i<=n2;i++)
+{
+if(n1%i==0 && n2%i==0)
+gcd=i;
+}
+System.out.println(gcd);
+}
+}
