96050c9c9319ec692f3a0b74d645de1130923ebe
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-07 15:11:27 +0530

    Update AllNumbers.java

--- AllNumbers.java
+++ AllNumbers.java
@@ -7,7 +7,7 @@ Scanner sc=new Scanner(System.in);
 String str=sc.nextLine();
 for(int i=0;i<str.length();i++)
 {
-if((Character.isDigit(str.charAt(i)))
+if(Character.isDigit(str.charAt(i)))
 System.out.print(str.charAt(i));
 }
 }
