ab6a1ac8cd614c5cf1c43b0eb68f60ca54ba632d
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-07 11:29:50 +0530

    Create NumberPresent.java

---
+++ NumberPresent.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class NumberPresent
+{
+public static void main(string args[])
+{
+Scanner sc=new Scanner(System.in);
+int count=0;
+int n=sc.nextInt();
+int k=sc.nextInt();
+int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+for(int i=0;i<n;i++)
+{
+if(count>0)
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
