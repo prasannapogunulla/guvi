f6eb6aa474026dda46ce0012572db1bd1a93c9a0
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-07 15:09:43 +0530

    Create AllNumbers.java

---
+++ AllNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class AllNumbers
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+for(int i=0;i<str.length();i++)
+{
+if((Character.isDigit(str.charAt(i)))
+System.out.print(str.charAt(i));
+}
+}
+}
