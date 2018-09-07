4b75174d423c072d73592b3952b9fb7c582c68b5
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-07 11:47:00 +0530

    Create PowerOfTwo.java

---
+++ PowerOfTwo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class PowerOfTwo
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int r=n*2;
+System.out.println(r);
+}
+}
