d29ec65b0ebe3074766d169ba1e060dd551ebc85
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-07 12:22:32 +0530

    Create NearestGreaterIntegre.java

---
+++ NearestGreaterIntegre.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class NearestGreaterInteger
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+double d=sc.nextDouble();
+System.out.println((int)Math.ceil(d));
+}
+}
