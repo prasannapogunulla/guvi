da4746eb6bcbf93f86c67f1ed1e20af0923148e0
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-08 11:41:15 +0530

    Create CelsiusToKelvin.java

---
+++ CelsiusToKelvin.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class CelsiusToKelvin
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m;
+		m=n+273;
+		System.out.println(m);
+		}
+}
