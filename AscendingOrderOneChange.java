2d1ce93f0f9ab0dfbb1101572d5deea5528820dc
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-08 11:48:27 +0530

    Create AscendingOrderOneChange.java

---
+++ AscendingOrderOneChange.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class AscendingOrderOneChange
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=in.nextInt();
+		}
+		int p=0,c=0;
+		for(int i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				if(a[i]>a[j]){
+					p=i;
+					c=1;
+				}
+			}
+		}
+		if(c==0)
+		System.out.print(n/2);
+		else
+		System.out.print(p+1);
+	}
+}
