31d68f505574889174b7bf13373f29179b380d35
prasannapogunulla <41738246+prasannapogunulla@users.noreply.github.com>
2018-09-08 11:32:23 +0530

    Update LastNcharacter.java

--- LastNcharacter.java
+++ LastNcharacter.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 class LastNcharacter
 {
-public static void main(String args[])
-{
-Scanner sc=new Scanner(System.in);
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String str;
+		Scanner sc=new Scanner(System.in);
+		str=sc.next();
+		int n,m;
+		m=sc.nextInt();
+		n=str.length();
+		for(int i=(n-m);i<n;i++){
+			System.out.print(str.charAt(i));
+			
+		}
+	}
+}
+
